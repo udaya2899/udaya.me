e0752eb4c6cd229dfae695fb21d9250eb202575f
Udaya N <udayaprakash2899@gmail.com>
2020-11-29 03:45:27 +0530

    Added path import

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs';
+import path from 'path';
 
 export default (req, res) => {
   var data = fs.readFileSync(
